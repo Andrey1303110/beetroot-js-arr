7026de6e80eb0ba26bc55216f66dafaa784852d8
Andrey1303110 <andrey1303110@gmail.com>
2020-11-29 17:01:56 +0200

    commit

--- js/main3.js
+++ js/main3.js
@@ -1,9 +1,23 @@
-function setname(name) {
-    document.write('Привет', name);
+function setName(firstName, lastName) {
+    document.write('Привет ', firstName, lastName, "<br />");
 }
 
-setname(name) {
-    name = 'Andrey';
+let setName2 = function setName2(firstName, lastName) {
+    document.write("Здравствуй ", firstName, lastName, "<br />");
 }
 
-setname();
+let setName3 = function setName3(firstName, lastName) {
+    document.write("Прощай ", firstName, lastName, "<br />")
+}
+
+setName(firstName = 'andrey', lastName = ' pastushenko');
+setName2(firstName, lastName = ' pastushenko');
+setName3(firstName, lastName = ' pastushenko')
+
+setName(firstName = 'stepan', lastName = ' pastushenko');
+setName2(firstName, lastName = ' pastushenko');
+setName3(firstName, lastName = ' pastushenko')
+
+setName(firstName = 'grisha', lastName = ' girilovich');
+setName2(firstName, lastName = ' girilovich');
+setName3(firstName, lastName = ' girilovich')
