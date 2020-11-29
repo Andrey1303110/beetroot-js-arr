4dbf62507b2c1096b4213041d9c174e5c94df06f
Andrey1303110 <andrey1303110@gmail.com>
2020-11-29 18:37:37 +0200

    commit

---
+++ js/main3.js
@@ -0,0 +1,36 @@
+function setName(firstName, lastName) {
+    document.write('Привет ', firstName, lastName, "<br />");
+}
+
+function setName2(firstName, lastName) {
+    document.write("Здравствуй ", firstName, lastName, "<br />");
+}
+
+function setName3(firstName, lastName) {
+    document.write("Прощай ", firstName, lastName, "<br /><br />")
+}
+
+setName(firstName = 'andrey', lastName = ' pastushenko');
+setName2(firstName, lastName = ' pastushenko');
+setName3(firstName, lastName = ' pastushenko')
+
+setName(firstName = 'stepan', lastName = ' pastushenko');
+setName2(firstName, lastName = ' pastushenko');
+setName3(firstName, lastName = ' pastushenko')
+
+setName(firstName = 'grisha', lastName = ' girilovich');
+setName2(firstName, lastName);
+setName3(firstName, lastName);
+
+setName(firstName = 'sobaka ', lastName = firstName);
+setName3(firstName, lastName);
+
+const pow1 = num => {
+    return num ** 2
+}
+
+document.write(pow1(num = 5));
+
+let setName4 = setName3(firstName = 'sanya ', lastName = 'nesanya');
+
+document.write(setName4());
