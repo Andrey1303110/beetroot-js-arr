086ed994085ece8e2128538990fffad84c51d0cb
Andrey1303110 <andrey1303110@gmail.com>
2020-11-29 21:26:55 +0200

    commit

--- js/array.js
+++ js/array.js
@@ -1,7 +1,6 @@
-let cars = ['mazda', 'ford', 'bmw', 'mercedes'];
+var cars = ['mazda', 'ford', 'bmw', 'mercedes'];
 
 cars[4] = 'porshe';
-cars[-1] = 'zaz';
 cars.push('ferarri');
 cars.unshift('renault');
 cars.shift();
@@ -9,7 +8,42 @@ cars.pop();
 
 console.log(cars);
 console.log(cars.reverse());
+console.log(cars.indexOf('ford'));
+
+var carsIndex = (cars.indexOf('ford'));
+
+console.log(carsIndex);
 
 var text = 'Я люблю тебя моё Солнышко';
 var revText = text.split('').reverse().join('');
 console.log(revText);
+
+var people = [
+    {
+        name: 'Stepa',
+        age: 47,
+        'english language': false
+    },
+    {
+        name: 'Andre',
+        age: 25,
+        'english language': true
+    },
+    {
+        name: 'Gosha',
+        age: 55,
+        'english language': false
+    }
+];
+
+let peopleInd = people.findIndex(function (person) {
+    return person.age == 55;
+})
+
+console.log(people[peopleInd]);
+
+let peopleName = people.findIndex(function (person) {
+    return person.name == 'Andre';
+})
+
+console.log(people[peopleName]);
